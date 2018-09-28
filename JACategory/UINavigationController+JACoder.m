f50708755fd6f11c4865c14ebe787e41fade0922
HZ-Jason <whz1992gt@gmail.com>
2018-09-28 18:29:34 +0800

    JACategory_1538130570

--- JACategory/UINavigationController+JACoder.m
+++ JACategory/UINavigationController+JACoder.m
@@ -27,7 +27,7 @@
     NSUInteger index = [self.viewControllers indexOfObject:self.topViewController];
     
     // TabBar 中的 viewControllers 也会触发, index == 0 导致崩溃
-    if (index <= 0) {return [self ja_popViewControllerAnimated:true];}
+    if (index <= 0) {return [self ja_popViewControllerAnimated:animated];}
     
     if (index == 1) {
         self.viewControllers[index - 1].hidesBottomBarWhenPushed = false;
@@ -35,7 +35,7 @@
         self.viewControllers[index - 1].hidesBottomBarWhenPushed = true;
     }
     
-    return [self ja_popViewControllerAnimated:true];
+    return [self ja_popViewControllerAnimated:animated];
 }
 
 - (UIImageView *)ja_findHairlineImageViewUnder:(UIView *)view {
