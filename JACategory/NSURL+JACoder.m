96c7efefc8f2baa30998b81bd082239d79548ae1
HZ-Jason <whz1992gt@gmail.com>
2018-11-15 18:21:19 +0800

    JACategory_1542277266

--- JACategory/NSURL+JACoder.m
+++ JACategory/NSURL+JACoder.m
@@ -11,14 +11,18 @@
 @implementation NSURL (JACoder)
 
 - (NSDictionary *)ja_queryPairs {
-    NSArray *qS = [self.query componentsSeparatedByString:@"&"];
-    
-    NSMutableDictionary *qSDicM = [NSMutableDictionary dictionaryWithCapacity:qS.count];
-    [qS enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSArray *paramPair = [obj componentsSeparatedByString:@"="];
-        [qSDicM setObject:paramPair.lastObject forKey:paramPair.firstObject];
-    }];
-    return [qSDicM copy];
+    if (self.query) {
+        NSArray *qS = [self.query componentsSeparatedByString:@"&"];
+        
+        NSMutableDictionary *qSDicM = [NSMutableDictionary dictionaryWithCapacity:qS.count];
+        [qS enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSArray *paramPair = [obj componentsSeparatedByString:@"="];
+            [qSDicM setObject:paramPair.lastObject forKey:paramPair.firstObject];
+        }];
+        return [qSDicM copy];
+    }else {
+        return nil;
+    }
 }
 
 @end
