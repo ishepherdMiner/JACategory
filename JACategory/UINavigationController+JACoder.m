e5c32602741f94686c25e29daa78dc9d18ad7f07
HZ-Jason <whz1992gt@gmail.com>
2018-12-10 19:32:54 +0800

    JACategory_1544441561

--- JACategory/UINavigationController+JACoder.m
+++ JACategory/UINavigationController+JACoder.m
@@ -53,19 +53,26 @@
 
 - (void)ja_setNeedsNavigationBackground:(CGFloat)alpha {
     // 导航栏背景透明度设置
-    UIView *barBackgroundView = [[self.navigationBar subviews] objectAtIndex:0];// _UIBarBackground
-    UIImageView *backgroundImageView = [[barBackgroundView subviews] objectAtIndex:0];// UIImageView
-    if (self.navigationBar.isTranslucent) {
-        if (backgroundImageView != nil && backgroundImageView.image != nil) {
-            barBackgroundView.alpha = alpha;
-        } else {
-            UIView *backgroundEffectView = [[barBackgroundView subviews] objectAtIndex:1];// UIVisualEffectView
-            if (backgroundEffectView != nil) {
-                backgroundEffectView.alpha = alpha;
+    // _UIBarBackground
+    if ([self.navigationBar subviews].count >= 1) {
+        UIView *barBackgroundView = [[self.navigationBar subviews] objectAtIndex:0];
+        if ([barBackgroundView subviews].count >= 1) {
+            UIImageView *backgroundImageView = [[barBackgroundView subviews] objectAtIndex:0];// UIImageView
+            if (self.navigationBar.isTranslucent) {
+                if (backgroundImageView != nil && backgroundImageView.image != nil) {
+                    barBackgroundView.alpha = alpha;
+                } else {
+                    if ([barBackgroundView subviews].count >= 2) {
+                        UIView *backgroundEffectView = [[barBackgroundView subviews] objectAtIndex:1];// UIVisualEffectView
+                        if (backgroundEffectView != nil) {
+                            backgroundEffectView.alpha = alpha;
+                        }
+                    }
+                }
+            } else {
+                barBackgroundView.alpha = alpha;
             }
         }
-    } else {
-        barBackgroundView.alpha = alpha;
     }
 }
 @end
